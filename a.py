cf5127d84bd94a9a8c19e63e4d16d0bb91aa600f
Fakunabs <gempro0501@gmail.com>
2023-07-02 16:41:35 +0700

    first commit

---
+++ a.py
@@ -0,0 +1,18 @@
+import h5py
+import pickle
+# Đọc đối tượng từ file pickle
+# with open('D:\Download\FashionGenAttnGAN\FashionGenAttnGAN\data\captions2_attngan.pickle', 'rb') as file:
+#     data = pickle.load(file)
+
+# print(len(data))
+# train_file_h5 = h5py.File("D:\Download\FashionGenAttnGAN\FashionGenAttnGAN\data\\fashiongen_256_256_train.h5", mode='r')
+
+# print(train_file_h5['image'][2])
+from PIL import ImageFont, ImageDraw
+import os
+current_dir = os.path.dirname(__file__)
+fnt = ImageFont.truetype("code\FreeMono.ttf", 50)
+# print the current path of os
+
+
+# print (os.path.join(current_dir, "code", "FreeMono.ttf"))
